47fb108f65b844584c2f62c4ef99fc91055a1cd2
MiraNovikova <universum.sm@gmail.com>
2024-06-07 20:27:55 +0300

    second com

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { link } = require('fs');
 const http = require('http');
  
 const hostname = '127.0.0.1';
@@ -12,3 +13,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
